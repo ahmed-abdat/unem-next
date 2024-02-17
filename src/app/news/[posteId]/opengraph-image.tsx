0093ef1f13e55d789a5a5b3fbde85ad6afbfb11d
= <ahmedeabdat@gmail.com>
2024-02-17 19:45:26 +0000

    Update opengraph-image component to include full width and height styling

--- src/app/news/[posteId]/opengraph-image.tsx
+++ src/app/news/[posteId]/opengraph-image.tsx
@@ -15,7 +15,7 @@ export default async function og({ params } : { params: { postId: string } }) {
     return new ImageResponse(
         (
             <div tw="relative flex items-center justify-center">
-            <img src={poste?.images[0].url} alt={poste?.description} />
+            <img src={poste?.images[0].url} alt={poste?.description} tw='flex w-full h-full'/>
             <div tw='absolute flex bg-gray-900 opcity-40 inset-0'></div>
             <div tw="absolute flex items-center bottom-2 w-full text-center">
                 <h2 tw='text-whtie text-4xl flex font-bold m-5'>{poste?.title}</h2>
