7c8be2b25dd1de8942987d9f9b912271b8f104a7
= <ahmedeabdat@gmail.com>
2024-02-19 23:21:08 +0000

    Add responsive image sizes to HeaderImage component

--- src/components/HeaderImage.tsx
+++ src/components/HeaderImage.tsx
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function HeaderImage({picture} : {picture: string}) {
   return (
       <header className="max-h-[17rem] mx-auto flex w-[94%] overflow-hidden" >
-        <Image src={picture} alt="header" width={450} height={300} className="object-contain mx-auto max-w-full max-h-full" priority={true} />
+        <Image src={picture} alt="header" width={450} height={300} className="object-contain mx-auto max-w-full max-h-full" priority={true}
+        sizes="(max-width: 450px) 100vw, (max-width: 768px) 50vw, 33vw"
+        />
       </header>
   );
 }
