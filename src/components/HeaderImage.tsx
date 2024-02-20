32d954774a72d7c30f1e24ad2fdf90e99ed7185a
= <ahmedeabdat@gmail.com>
2024-02-20 16:10:43 +0000

    Update HeaderImage sizes

--- src/components/HeaderImage.tsx
+++ src/components/HeaderImage.tsx
@@ -4,7 +4,7 @@ export default function HeaderImage({picture} : {picture: string}) {
   return (
       <header className="max-h-[17rem] mx-auto flex w-[94%] overflow-hidden" >
         <Image src={picture} alt="header" width={450} height={300} className="object-contain mx-auto max-w-full max-h-full" priority={true}
-        sizes="(max-width: 450px) 100vw, (max-width: 768px) 50vw, 33vw"
+       sizes="(max-width: 768px) 80vw, (max-width: 1200px) 50vw, 33vw"
         />
       </header>
   );
