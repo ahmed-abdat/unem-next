638d7e83de3a2c64b8a040595b83e81fa114a815
= <ahmedeabdat@gmail.com>
2024-02-27 23:21:27 +0000

    Fix typo in variable name and update description field

--- src/app/news/[posteId]/opengraph-image.tsx
+++ src/app/news/[posteId]/opengraph-image.tsx
@@ -27,7 +27,7 @@ export default async function og({ params } : { params: { postId: string } }) {
             <div tw="relative flex items-center justify-center" style={{
                 backgroundColor : '#495057'
             }}>
-            <img src={poste?.images[0].url} alt={poste?.description} tw='flex w-40 h-40'/>
+            <img src={poste?.images[0].url} alt={poste?.discribtion} tw='flex w-40 h-40'/>
             <div tw='absolute flex bg-black opcity-40 inset-0'></div>
             <div tw="absolute flex items-center bottom-2 w-full text-center">
                 <h2 tw='text-whtie text-4xl flex font-bold m-5'>{poste?.title}</h2>
