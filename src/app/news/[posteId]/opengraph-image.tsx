b4828b824ba4987b014ad0daf0698db74dc97453
= <ahmedeabdat@gmail.com>
2024-03-02 22:10:10 +0000

    Fix bug in login functionality

--- src/app/news/[posteId]/opengraph-image.tsx
+++ src/app/news/[posteId]/opengraph-image.tsx
@@ -1,41 +1,37 @@
-import { getPoste , getAllPostes } from "@/app/action"
+import { getPoste } from '@/app/action'
 import { ImageResponse } from 'next/og'
-
-export const alt = 'الأخبار والمنشورات'
+ 
+export const runtime = 'edge'
+ 
+export const alt = 'About Acme'
 export const size = {
-    width: 1200,
-    height: 630,
-  }
-
-export const contentType = 'image/png'
-
-export async function generateStaticParams() {
-    const postes = await getAllPostes();
-    return postes.map((poste) => ({
-        params: {
-        postId: poste.id,
-        },
-    }));
-    
+  width: 1200,
+  height: 630,
 }
-
+export const contentType = 'image/png'
+ 
 export default async function og({ params } : { params: { postId: string } }) {
     const { postId } = params
     const {poste} = await getPoste(postId)
-    return new ImageResponse(
-        (
-            <div tw="relative flex items-center justify-center" style={{
-                backgroundColor : '#495057'
-            }}>
-            <img src={poste?.thumbnail?.url || poste?.images[0].url || '/logo.png'} alt={poste?.discribtion} tw='flex w-40 h-40'/>
-            <div tw='absolute flex bg-black opcity-40 inset-0'></div>
-            <div tw="absolute flex items-center bottom-2 w-full text-center">
-                <h2 tw='text-whtie text-4xl flex font-bold m-5'>{poste?.title}</h2>
-            </div>
-        </div>
-        ),
-        {
-            ...size,
-          }
-    )
+ 
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 48,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {poste?.title}
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
 }
