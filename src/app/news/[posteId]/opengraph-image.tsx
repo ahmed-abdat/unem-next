8c23f3e6dc6e435cf6d40ea026955ed1be53b1d3
= <ahmedeabdat@gmail.com>
2024-03-02 22:13:28 +0000

    Refactor opengraph-image component

--- src/app/news/[posteId]/opengraph-image.tsx
+++ src/app/news/[posteId]/opengraph-image.tsx
@@ -16,18 +16,23 @@ export default async function og({ params } : { params: { postId: string } }) {
  
   return new ImageResponse(
     (
-      <div
-        style={{
-          fontSize: 48,
-          background: 'white',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        {poste?.title}
+        <div tw="flex flex-col w-full h-full items-center justify-center bg-white">
+        <div tw="bg-gray-50 flex w-full">
+          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+              <span>Ready to dive in?</span>
+              <span tw="text-indigo-600">Start your free trial today.</span>
+            </h2>
+            <div tw="mt-8 flex md:mt-0">
+              <div tw="flex rounded-md shadow">
+                <a tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white">Get started</a>
+              </div>
+              <div tw="ml-3 flex rounded-md shadow">
+                <a tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600">Learn more</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     ),
     {
