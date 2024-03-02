8a607889b8fda29198dfc6865eef357468765a96
= <ahmedeabdat@gmail.com>
2024-03-02 16:26:24 +0000

    Fix typo in footer text

--- src/components/news/Footer.tsx
+++ src/components/news/Footer.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="w-full flex flex-col gap-y-8 bg-white py-10">
-      <h2 className="text-center font-aljazira font-medium text-[1.125rem] text-gray-800">تابع الإتحاد الوطني على : </h2>
+      <h2 className="text-center font-aljazira font-medium text-[1.125rem] text-gray-800">تابع الاتحاد الوطني على : </h2>
       <FooterSocial />
       <Link href={'/'} className="flex items-center justify-center" aria-label="home">
         <Image src={logo} alt={'logo'} width={100} height={100} placeholder="blur" className="object-cover object-center" />
