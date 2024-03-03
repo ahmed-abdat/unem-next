a791a94648f9e3fceb76fcde56e6cba41a6f0b8d
= <ahmedeabdat@gmail.com>
2024-03-03 21:41:32 +0000

    Update animation coordinates in Template component

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -16,9 +16,9 @@ function FrozenRouter(props: { children: React.ReactNode }) {
 }
 
 const variants = {
-  hidden: { opacity: 0, x: -100, y: 0 },
+  hidden: { opacity: 0, x: -200, y: 0 },
   enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: -100 },
+  exit: { opacity: 0, x: 0, y: -200 },
 };
 export default function Template({ children }: { children: React.ReactNode }) {
   const key = usePathname();
