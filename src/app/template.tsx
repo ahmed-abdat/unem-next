a6dd16f17ebbc1c9848b2bfd3eec676de5758401
= <ahmedeabdat@gmail.com>
2024-03-03 21:33:52 +0000

    Add framer-motion library to package.json

---
+++ src/app/template.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { motion, AnimatePresence } from "framer-motion";
+import { usePathname } from "next/navigation";
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useContext, useRef } from "react";
+
+function FrozenRouter(props: { children: React.ReactNode }) {
+  const context = useContext(LayoutRouterContext);
+  const frozen = useRef(context).current;
+
+  return (
+    <LayoutRouterContext.Provider value={frozen}>
+      {props.children}
+    </LayoutRouterContext.Provider>
+  );
+}
+
+const variants = {
+  hidden: { opacity: 0, x: -100, y: 0 },
+  enter: { opacity: 1, x: 0, y: 0 },
+  exit: { opacity: 0, x: 0, y: -100 },
+};
+export default function Template({ children }: { children: React.ReactNode }) {
+  const key = usePathname();
+
+  return (
+    <AnimatePresence mode="popLayout">
+      <motion.div
+        key={key}
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+          duration: 0.2,
+          delay: 0.1,
+        }}
+      >
+        <FrozenRouter>{children}</FrozenRouter>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
