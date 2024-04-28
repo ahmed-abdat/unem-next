cc9874dd6bc6875d1f625811bb8564dcd32964ee
ahmed-abdat <ahmedeabdat@gmail.com>
2024-04-28 18:41:42 +0100

    Update accademic layout metadata

--- src/app/accademic/layout.tsx
+++ src/app/accademic/layout.tsx
@@ -1,5 +1,12 @@
 import NewsHeader from "@/components/news/header/NewsHeader";
 import NewsFooter from "@/components/news/Footer";
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+    title: 'الشؤون الأكاديمية',
+    description: "الاتحاد الوطني لطلبة موريتانيا - الشؤون الأكاديمية",
+  };
 
 
 export default function RootLayout({
