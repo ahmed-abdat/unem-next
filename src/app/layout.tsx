e3c0c5e6252743e681323f6ec06ff0d1e7308769
ahmed-abdat <ahmedeabdat@gmail.com>
2024-05-27 19:35:14 +0100

    chore: Update dependencies and add TipTap editor to Poste component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
 import type { Metadata, Viewport } from "next";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import {tajawal , Aljazira} from '@/app/font/font'
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { tajawal, Aljazira } from "@/app/font/font";
 import { Toaster } from "sonner";
 import "./globals.css";
+import { CSPostHogProvider } from "@/app/PostHugProvider";
 
 export const revalidate = 120;
 
 export const metadata: Metadata = {
-  metadataBase : new URL('https://unem2000.com'),
-  manifest: '/manifest.json',
+  metadataBase: new URL("https://unem2000.com"),
+  manifest: "/manifest.json",
   title: {
-    default : 'الاتحاد الوطني لطلبة موريتانيا',
-    template : '%s | الاتحاد الوطني',
+    default: "الاتحاد الوطني لطلبة موريتانيا",
+    template: "%s | الاتحاد الوطني",
   },
   description: "الاتحاد الوطني لطلبة موريتانيا - الأخبار والمنشورات",
 };
 
-export const viewport : Viewport = {
-themeColor : '#58cc02',
-}
-
+export const viewport: Viewport = {
+  themeColor: "#58cc02",
+};
 
 export default function RootLayout({
   children,
@@ -28,11 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ar" dir="rtl">
-      <body className={`${Aljazira.className}  ${tajawal.className} bg-main`}>
-      {children}
-        <Toaster position="top-center" richColors />
-        <SpeedInsights/>
-      </body>
+      <CSPostHogProvider>
+        <body className={`${Aljazira.className}  ${tajawal.className} bg-main`}>
+          {children}
+          <Toaster position="top-center" richColors />
+          <SpeedInsights />
+        </body>
+      </CSPostHogProvider>
     </html>
   );
 }
